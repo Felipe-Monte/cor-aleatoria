3f65d5922de01d6f2352868191a5e440dc4371c0
felipe <carlosfelipemonte1997@hotmail.com>
2024-11-22 10:45:30 -0300

    Feat: create random color in game

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const allBoxes = document.querySelectorAll('.item');
+let errors = 0;
+let successes  = 0;
+
+let lastIndex = -1; // Armazena o índice da última caixa selecionada
 
 function randomBoxColor() {
+  // Limpa as cores de todas as caixas
   allBoxes.forEach((box) => {
     box.style.backgroundColor = '';
   });
 
-  const randomIndex = Math.floor(Math.random() * allBoxes.length);
-  const randomBox = allBoxes[randomIndex];
+  let randomIndex;
 
+  // Gera um novo índice enquanto for igual ao último
+  do {
+    randomIndex = Math.floor(Math.random() * allBoxes.length);
+  } while (randomIndex === lastIndex);
+
+  // Atualiza o índice da última caixa selecionada
+  lastIndex = randomIndex;
+
+  // Define a cor da caixa selecionada
+  const randomBox = allBoxes[randomIndex];
   randomBox.style.backgroundColor = 'red';
 }
 
 randomBoxColor();
 
-//Se a caixa clicada for vermelha mostre "acertou" no console se nao "errou"
+allBoxes.forEach((box) => {
+  box.addEventListener('click', function () {
+    if (box.style.backgroundColor === 'red') {
+      randomBoxColor();
+      successes ++;
+    } else {
+      errors++;
+    }
+
+    console.log(`Erros: ${errors} Acertos: ${successes }`);
+  });
+});
+
